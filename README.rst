658cef400cf56ae08d2b9bb8fad360a86c712f4e
RF-Tar-Railt <3165388245@qq.com>
2022-07-04 00:41:35 +0800

    update setup

---
+++ README.rst
@@ -0,0 +1,34 @@
+Unwind
+======
+
+A simple solution to analysis and extract information from traceback
+
+usage
+-----
+
+.. code:: python
+
+   from graiax.mod.unwind import Report
+
+   with Report() as report:
+       a = 1
+       b = 2
+       c = 'a'
+       d = 1 + c
+
+   print(report.errors)
+   print(report.reports)
+
+or
+
+.. code:: python
+
+   from graiax.mod.unwind import get_report
+
+   try:
+       a = 1
+       b = 2
+       c = 'a'
+       d = 1 + c
+   except Exception as e:
+       print(get_report(e))
---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.rst", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="graiax-mod-unwind",
+    version="0.1.0",
+    author="RF-Tar-Railt",
+    author_email="rf_tar_railt@qq.com",
+    description="A simple solution to analysis and extract information from traceback.",
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type="text/rst",
+    url="https://github.com/GraiaCommunity/Unwind",
+    packages=["graiax.mod.unwind"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
+    ],
+    keywords=['traceback', 'exception', 'crash-report'],
+    python_requires='>=3.8',
+    project_urls={
+        'Bug Reports': 'https://github.com/GraiaCommunity/Unwind/issues',
+        'Source': 'https://github.com/GraiaCommunity/Unwind',
+    },
+)
